c6df25cbacfc9e0f90c905ea6c32f247de3a2445
Do Hoang Hieu With Hangg <dohoanghieu7537@gmail.com>
2026-03-22 12:20:55 +0700

    shared

---
+++ wmss/shared/.eslintrc.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  env: { node: true },
+  ignorePatterns: ['dist', 'node_modules']
+};
