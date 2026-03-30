441ee785540b5225659ea786ffbe363ac4abeb95
PhiHungfVox <23520582@gm.uit.edu.vn>
2026-03-30 20:20:30 +0700

    up

---
+++ wmss/server/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier'
+  ],
+  env: {
+    node: true,
+    jest: true
+  },
+  rules: {
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false
+        }
+      }
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': 'warn'
+  },
+  ignorePatterns: ['dist', 'node_modules']
+};
